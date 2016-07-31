3e6c92c9459221d3250a1d6aad692bdb0845f991
randelramirez <randelramirez1@gmail.com>
2016-08-01 00:16:18 +0800

    Added SplittingEntityAmongMultipleTables(Vertical Splitting)

---
+++ SplittingEntityAmongMultipleTables/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplittingEntityAmongMultipleTables
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string  FavoriteColor { get; set; }
+
+        public string FavoriteFood { get; set; }
+
+        public string FavoriteQuote { get; set; }
+    }
+}
