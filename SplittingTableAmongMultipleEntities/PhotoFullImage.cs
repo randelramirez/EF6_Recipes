8107cec5ad1c6cdf926bcb3e6ef5f8c5218b57b7
randelramirez <randelramirez1@gmail.com>
2016-08-02 01:13:07 +0800

    Added SplittingTableAmongMultipleEntities project

---
+++ SplittingTableAmongMultipleEntities/PhotoFullImage.cs
@@ -0,0 +1,17 @@
+﻿namespace SplittingTableAmongMultipleEntities
+{
+    public class PhotoFullImage
+    {
+        // using Data annotation without Fluent API
+        //[Key] // This attribute would be required if the Id/primary key does not follow the convention, eg: PhotoId or FooId => does not follow convention, Id & PhotoFullImageId => follows the convention
+        //[ForeignKey("PhotoThumbnail")]
+        public int Id { get; set; }
+
+        public byte[] HighResolution { get; set; }
+
+        // using Data annotation without Fluent API
+        //[ForeignKey("Id")]  //[ForeignKey("Id")] // not working using this style, check why?
+        public virtual PhotoThumbnail PhotoThumbnail { get; set; }
+    }
+}
+
