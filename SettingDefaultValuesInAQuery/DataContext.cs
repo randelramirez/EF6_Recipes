471df415b85f3aeadefa0321bab1a7532fad705e
randelramirez <randelramirez1@gmail.com>
2016-08-10 23:18:10 +0800

    clean up

--- SettingDefaultValuesInAQuery/DataContext.cs
+++ SettingDefaultValuesInAQuery/DataContext.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SettingDefaultValuesInAQuery
+﻿using System.Data.Entity;namespace SettingDefaultValuesInAQuery
 {
     public class DataContext : DbContext
     {
--- SettingDefaultValuesInAQuery/Employee.cs
+++ SettingDefaultValuesInAQuery/Employee.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SettingDefaultValuesInAQuery
+﻿namespace SettingDefaultValuesInAQuery
 {
     public class Employee
     {
-        public int EmployeeId { get; set; }
+        public int Id { get; set; }
+
         public string Name { get; set; }
+
         public int? YearsWorked { get; set; }
     }
 }
